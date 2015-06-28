8e2a72be3140bb9d1cb3fe7e9a2b1f06a97b5642
Pranav Ashok <pranavashok@gmail.com>
2015-06-28 18:20:58 +0530

    Added shift keys and its functionality

--- keyboard.js
+++ keyboard.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+	var shift = 0;
     var spans = document.getElementsByTagName('span');
     for(var i = 0; i < spans.length; i++) {
         var span = spans[i];
@@ -12,6 +13,69 @@ window.onload = function() {
 	            document.getElementById('typearea').value = "";
             }
         }
+        else if(('KeyboardKey shiftkey').match(span.className)) {
+            span.onclick = function() {
+            	if(shift == 0) {
+			    	shiftpress();
+			    	shift = 1;
+			    }
+			    else {
+			     	shiftrelease();
+			     	shift = 0;
+			    }
+	        }
+	    }
+    }
+    document.onkeydown = (function (ev) {
+		  var key;
+		  var isShift;
+		  if (window.event) {
+		    key = window.event.keyCode;
+		    alert(key);
+		    isShift = !!window.event.shiftKey; // typecast to boolean
+		  } else {
+		    key = ev.which;
+		    isShift = !!ev.shiftKey;
+		  }
+		  if ( isShift ) {
+		    switch (key) {
+		      case 16:
+		      	if(shift == 0) {
+		      		shiftpress();
+		      		shift = 1;
+		      	}
+		      	else {
+		      		shiftrelease();
+		      		shift = 0;
+		      	}
+		        break;
+		      default:
+		        //alert(key);
+		        break;
+		    }
+		  }
+		});
+}
+
+function shiftpress() {
+	var nonshifts = document.getElementsByClassName("nonshift");
+    for(var i = 0; i < nonshifts.length; i++) {
+    	nonshifts[i].style.display = 'none';
+    }
+    var shifts = document.getElementsByClassName("shift");
+    for(var i = 0; i < nonshifts.length; i++) {
+    	shifts[i].style.display = 'block';
+    }
+}
+
+function shiftrelease() {
+	var nonshifts = document.getElementsByClassName("nonshift");
+    for(var i = 0; i < nonshifts.length; i++) {
+    	nonshifts[i].style.display = 'block';
+    }
+    var shifts = document.getElementsByClassName("shift");
+    for(var i = 0; i < nonshifts.length; i++) {
+    	shifts[i].style.display = 'none';
     }
 }
 
